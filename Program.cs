bf43e2dea1f5697e9a1f1a5d455bd887d7fc8b1c
Sergey Edalov <edalov94@gmail.com>
2023-03-09 13:08:59 +0300

    add create massive

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿int EnterData(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+string[] FillArray(string[] matr)
+{
+    string elem = String.Empty;
+    for (int i = 0; i < matr.Length; i++)
+    {
+        Console.WriteLine($"Enter {i+1} element massive");
+        elem = Console.ReadLine();
+        matr[i] = elem;
+    }
+    Console.WriteLine($"Massive = [{string.Join(", ", matr)}]");
+    return matr;
+}
+
+
+
+int m = EnterData("Enter size massive");
+string[] array = new string[m];
+
+FillArray(array);
+
+
+
+
+
